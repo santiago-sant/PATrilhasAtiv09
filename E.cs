ec4ddee0bfcf8ed44d6132edebab10eaa6052f1b
Alvaro Ramos <alvarosantiagosilva03@gmail.com>
2025-02-10 10:26:00 -0300

    Create E.cs

---
+++ E.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+    	int[] vetor = {10,84,75,46,50};
+    	int maior = vetor.Min();
+    	
+    	Console.WriteLine("O menor número e: " + maior);
+    		
+    	
+    	
+        Console.Write("Press any key to continue . . . ");
+        Console.ReadKey(true);
+  
+    }
+}
